25a8d4207b8399272eb9d335c7dec869af868c43
dy <48380963+dyrisingsunlight@users.noreply.github.com>
2019-10-06 22:43:13 +0800

    Dy

---
+++ AcWing279自然数拆分/Code.cpp
@@ -0,0 +1,38 @@
+//Coded by Dy.
+#include<iostream>
+#include<cstdio>
+#include<cstring>
+#include<algorithm>
+#define gc getchar()
+#define pc(x) putchar(x)
+typedef long long ll;
+const ll mod = 2147483648;
+inline int sc() {
+    int xx = 0, ff = 1; char cch = gc;
+    while(!isdigit(cch)) {
+        if(cch == '-') ff = -1; cch = gc;
+    }
+    while(isdigit(cch)) {
+        xx = (xx << 1) + (xx << 3) + (cch ^ '0'); cch = gc;
+    }
+    return xx * ff;
+}
+inline void out(ll x) {
+    if(x < 0)
+        pc('-'), x = -x;
+    if(x >= 10)
+        out(x / 10);
+    pc(x % 10 + '0');
+}
+#define re register
+int n;
+ll f[4010];
+int main() {
+	n = sc();
+	f[0] = 1;
+	for(re int i = 1; i < n; ++i)
+		for(re int j = i; j <= n; ++j)
+			f[j] = (f[j] + f[j - i]) % mod;
+	out(f[n]), pc('\n');
+    return 0;
+}
