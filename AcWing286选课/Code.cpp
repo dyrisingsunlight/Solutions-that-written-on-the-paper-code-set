2b3309922867378dd1f1f5f9a087020fc091b4b6
Dy <48380963+dyrisingsunlight@users.noreply.github.com>
2019-11-04 19:04:26 +0800

    Dy

---
+++ AcWing286选课/Code.cpp
@@ -0,0 +1,63 @@
+//Coded by Dy.
+#include<iostream>
+#include<cstdio>
+#include<cstring>
+#include<algorithm>
+#define gc getchar()
+#define pc(x) putchar(x)
+inline int sc() {
+	int xx = 0, ff = 1; char cch = gc;
+	while(!isdigit(cch) && cch != EOF) {
+		if(cch == '-')
+			ff = -1; cch = gc;
+	}
+	while(isdigit(cch)) {
+		xx = (xx << 1) + (xx << 3) + (cch ^ '0'); cch = gc;
+	}
+	return xx * ff;
+}
+inline void out(int x) {
+	if(x < 0)
+		pc('-'), x = -x;
+	if(x >= 10)
+		out(x / 10);
+	pc(x % 10 + '0');
+}
+const int Maxn = 310;
+struct TREE {
+	int nxt, to;
+}t[Maxn << 1];
+int n, m, cnt;
+int val[Maxn], head[Maxn];
+int f[Maxn][Maxn];
+inline void ADD(int from, int to) {
+	t[++cnt].nxt = head[from];
+	t[cnt].to = to;
+	head[from] = cnt;
+}
+inline void dfs(int now) {
+	f[now][0] = 0;
+	for(int i = head[now]; i; i = t[i].nxt) {
+		int to = t[i].to;
+		dfs(to);
+		for(int j = m; j >= 0; --j)
+			for(int k = j; k >= 0; --k)
+				if(j - k >= 0)
+					f[now][j] = std :: max(f[now][j], f[now][j - k] + f[to][k]);
+	}
+	if(now)
+		for(int i = m; i >= 0; --i)
+			f[now][i] = f[now][i - 1] + val[now];
+}
+int main() {
+	memset(f, 0xcf, sizeof(f));
+	n = sc(), m = sc();
+	for(int i = 1; i <= n; ++i) {
+		int x = sc(), y = sc();
+		ADD(x, i);
+		val[i] = y;
+	}
+	dfs(0);
+	printf("%d\n", f[0][m]);
+	return 0;
+}
